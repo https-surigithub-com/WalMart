24f8afb3e70e05ecc3c302aa52ced9da88463630
SurendraReddy53 <suri1253@gmail.com>
2021-10-14 23:52:35 +0530

    First Commit

---
+++ StringUtils.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class StringUtils
+{
+	public static void main(String args[])
+	{
+		System.out.println("this file contains a String Utils code");
+	}
+}
+
---
+++ dbutils.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class dbutils
+{
+	public static void main(String args[])
+	{
+		System.out.println("this is dbutils program reg......");
+	}
+}
+
